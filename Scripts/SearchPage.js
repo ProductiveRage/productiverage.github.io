38233dec0051fb86ee2d642717df2827dd1a7336
Dan Roberts <dangger36@gmail.com>
2025-03-25 16:23:12 +0000

    Incorporate semantic search endpoint

--- Scripts/SearchPage.js
+++ Scripts/SearchPage.js
@@ -33,15 +33,54 @@
 	elementToReplaceWithResult = loading;
 
 	(function () {
-		var request = new XMLHttpRequest();
-		request.open("GET", "/SearchIndex-SummaryDictionary.js", true);
-		request.onload = function () {
+		var startedSearchAt = Date.now();
+		var semanticTimeoutInMs, arrSemanticResults;
+		if (document.location.hostname !== "www.productiverage.com") {
+			// We can only hit the semantic search subdomain from www.productiverage.com
+			semanticTimeoutInMs = 0;
+			arrSemanticResults = null;
+			Log("SearchPage.js: Semantic search not supported from this domain");
+		} else {
+			semanticTimeoutInMs = 1500;
+			arrSemanticResults = null;
+
+			var semanticRequest = new XMLHttpRequest();
+			semanticRequest.open("GET", "https://search.productiverage.com/?q=" + encodeURIComponent(strSearchTerm));
+			semanticRequest.setRequestHeader("Content-type", "application/json");
+			semanticRequest.timeout = semanticTimeoutInMs;
+			semanticRequest.onload = function () {
+				if (this.status >= 200 && this.status < 400) {
+					arrSemanticResults = JSON.parse(this.response).results;
+				}
+			};
+			semanticRequest.ontimeout = function () {
+				Log("SearchPage.js: Semantic search timed out");
+			};
+			semanticRequest.onerror = function () {
+				Log("SearchPage.js: Semantic search error");
+			};
+			semanticRequest.send();
+		}
+
+		var lexicalRequest = new XMLHttpRequest();
+		lexicalRequest.open("GET", "/SearchIndex-SummaryDictionary.js", true);
+		lexicalRequest.onload = function () {
 			if (this.status >= 200 && this.status < 400) {
+				function WaitForSemanticBeforeContinuing() {
+					var timeWillingToWaitForSemantic = (startedSearchAt + semanticTimeoutInMs) - Date.now();
+					if ((arrSemanticResults !== null) || (timeWillingToWaitForSemantic <= 0)) {
+						SearchIndexFor(objSearchIndexSummaryData, arrSemanticResults, strSearchTerm);
+						return;
+					}
+					setTimeout(WaitForSemanticBeforeContinuing, 100);
+				}
+
 				Log("SearchPage.js: Loaded summary index");
-				SearchIndexFor(JSON.parse(this.response), strSearchTerm);
+				var objSearchIndexSummaryData = JSON.parse(this.response);
+				WaitForSemanticBeforeContinuing();
 			}
 		};
-		request.send();
+		lexicalRequest.send();
 	})();
 	
 	function ReplaceWith(elementToReplace, newElement) {
@@ -49,9 +88,28 @@
 		elementToReplace.remove();
 	}
 
-	function SearchIndexFor(objSearchIndexSummaryData) {
+	function SearchIndexFor(objSearchIndexSummaryData, arrSemanticResults, strSearchTerm) {
 		var arrMatches = IndexSearchGenerator.Get(objSearchIndexSummaryData).SearchFor(strSearchTerm);
-		Log("SearchPage.js: Identified " + arrMatches.length + " match" + (arrMatches.length === 1 ? "" : "es"));
+		Log("SearchPage.js: Identified " + arrMatches.length + " lexical match" + (arrMatches.length === 1 ? "" : "es"));
+		if (arrSemanticResults) {
+			Log("SearchPage.js: Identified " + arrSemanticResults.length + " semantic match" + (arrSemanticResults.length === 1 ? "" : "es"));
+			if (arrSemanticResults.length > 0) {
+				var arrLexicalMatches = arrMatches;
+				arrMatches = [];
+				var arrSemanticResultKeys = [];
+				for (var i = 0; i < arrSemanticResults.length; i++) {
+					var intSemanticResultKey = arrSemanticResults[i].id;
+					arrSemanticResultKeys.push(intSemanticResultKey);
+					arrMatches.push({ Key: intSemanticResultKey, SourceLocations: [] });
+				}
+				for (var i = 0; i < arrLexicalMatches.length; i++) {
+					var objLexicalMatch = arrLexicalMatches[i];
+					if (!arrSemanticResultKeys.includes(objLexicalMatch.Key)) {
+						arrMatches.push(objLexicalMatch);
+					}
+				}
+			}
+		}
 		if (arrMatches.length === 0) {
 			var noResults = document.createElement("p");
 			noResults.className = "NoResults";
@@ -59,6 +117,7 @@
 			ReplaceWith(elementToReplaceWithResult, noResults);
 			return;
 		}
+
 		LoadCompressedJsonData(
 			"SearchIndex-Titles.lz.txt",
 			function (objTitles) {
@@ -103,19 +162,16 @@
 							strHtmlContent = "Unable to load content";
 						}
 						else {
+							// For lexical searches, there should be precisely one match since each detail file is for a single Post
+							// and we know that it matches this Post (according to the summary file, at least) - and there shouldn't
+							// be multiple entries for the same Post. However, if we got the match through a semantic search then we
+							// might not be able to match the search terms back to tokens in the detail file, and so there MAY not
+							// be any matches (in which case we'll resort to showing the first chunk of the text content).							
 							var arrMatches = IndexSearchGenerator.Get(objSearchIndexDetailDataForPost).SearchFor(strSearchTerm);
-							if (arrMatches.length < 0) {
-								strHtmlContent = "Unable to load content";
-							}
-							else {
-								// There should be precisely one match since each detail file is for a single Post and we know that it
-								// matches this Post (according to the summary file, at least) and there shouldn't be multiple entries
-								// for the same Post.
-								strHtmlContent = GetHighlightedPostSummary(
-									strPlainTextContent,
-									arrMatches[0].SourceLocations
-								);
-							}
+							strHtmlContent = GetHighlightedPostSummary(
+								strPlainTextContent,
+								arrMatches.length == 0 ? [] : arrMatches[0].SourceLocations
+							);
 						}
 						summary.innerHTML = strHtmlContent;
 						summary.style.transition = "opacity 1s";
